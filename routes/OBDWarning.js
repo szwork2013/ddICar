021e1a76d2f19d29f166bdf18828b2166e51e49f
AmberGlasses <775837740@qq.com>
2014-10-14 14:12:25 +0800

    debug

--- routes/OBDWarning.js
+++ routes/OBDWarning.js
@@ -21,7 +21,7 @@ exports.sendWarning = function (req, res) {
                         "target_type": "users", //or chatgroups
                         "target": [user.info.phone], //注意这里需要用数组, 即使只有一个用户, 也要用数组 ['u1']
                         "msg": {
-                            "type": "txt",
+                            "type": "json",
                             "msg": {type:"warning",content:obdErrorCode.mean} //消息内容，参考[聊天记录](http://developer.easemob.com/docs/emchat/rest/chatmessage.html)里的bodies内容
                         },
                         "from": "admin" //表示这个消息是谁发出来的, 可以没有这个属性, 那么就会显示是admin, 如果有的话, 则会显示是这个用户发出的
