0aa652fe472cffc74f8f6daa82dfa46f0c711099
AmberGlasses <775837740@qq.com>
2014-11-27 16:47:57 +0800

    debug

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ var pkg = require('./package.json'),
 
 config = {
     wx: {
-        appKey: 'wxfac56e93388fd4a0',
-        appSecret: 'c9c1a7662202fcd9183fa5774ac1eaa6',
+        appid: 'wxfac56e93388fd4a0',
+        secret: 'c9c1a7662202fcd9183fa5774ac1eaa6',
         tokenUrl: 'https://api.weixin.qq.com/sns/oauth2/access_token?',
         userInfoUrl: 'https://api.weixin.qq.com/sns/userinfo?'
     }
