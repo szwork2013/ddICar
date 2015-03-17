5ba934a2ec2d3762e9d3f4ac030450295ddcd6f4
AmberGlasses <775837740@qq.com>
2015-03-17 14:53:32 +0800

    debug

--- routes/signs.js
+++ routes/signs.js
@@ -15,7 +15,7 @@ exports.save = function (req, res) {
     then(function (defer) {
         console.log(1);
 
-        var newSign = new sign({user_id:user_id,longitude: longitude, latitude: latitude, type: type});
+        var newSign = new sign({user_id: user_id, longitude: longitude, latitude: latitude, type: type});
         console.log(2);
         newSign.save(defer);
     }).then(function (defer, result) {
@@ -32,18 +32,19 @@ exports.getByPosition = function (req, res) {
     var user_id = req.body.user_id;
     var longitude = req.body.longitude;
     var latitude = req.body.latitude;
+    var type = req.body.type;
 
-    res.json(Common.success({signs: [
-        {latitude: 39.908420, longitude: 116.397475},
-        {latitude: 39.907653, longitude: 116.391775}
-    ]}));
+    //res.json(Common.success({signs: [
+    //    {latitude: 39.908420, longitude: 116.397475},
+    //    {latitude: 39.907653, longitude: 116.391775}
+    //]}));
 
-//    then(function (defer) {
-//        var query = {name: { $regex: roomname, $options: 'i' } };
-//        Room.getQuery(query, defer);
-//    }).then(function (defer, result) {
-//        res.json(Common.success(result));
-//    }).fail(function (defer, err) {
-//        res.json(Common.fail(Common.commonEnum.SYSTEM_ERROR, "系统错误"));
-//    });
+    then(function (defer) {
+        var query = {type: type};
+        sign.getQuery(query, defer);
+    }).then(function (defer, result) {
+        res.json(Common.success(result));
+    }).fail(function (defer, err) {
+        res.json(Common.fail(Common.commonEnum.SYSTEM_ERROR, "系统错误"));
+    });
 };
